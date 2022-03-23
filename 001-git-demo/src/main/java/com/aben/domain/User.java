c59f531b079e32321acb2df1b0679bedc025b172
aben <2682480452@qq.com>
2022-03-23 16:47:25 +0800

    version 1.0.0

---
+++ 001-git-demo/src/main/java/com/aben/domain/User.java
@@ -0,0 +1,28 @@
+package com.aben.domain;
+
+/**
+ * @Author leng
+ * @Date 2022/3/23 16:41
+ * @ClassName User
+ * @Decription TOOD
+ **/
+public class User {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
